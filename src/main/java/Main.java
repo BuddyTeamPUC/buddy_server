7072875b65ff9ae72f193b186ba5856c395ebc26
Pedro Pereira Lourenço <ti2grupopuc@gmail.com>
2021-11-30 21:07:08 -0300

    changed main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,12 @@ import buddyserver.Server.ServerObject;
 
 public class Main {
 public static void main(String[] args) {
+	
+		
+		for(int i = 0; i < args.length; i++) 
+		{
+			System.out.println(args[i]);
+		}
 		
 		ServerObject server = new ServerObject(3000);
 		try {
