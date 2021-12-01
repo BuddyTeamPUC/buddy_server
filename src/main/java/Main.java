1dd95216cbda81e568cd95a33f2a87abed9f9185
Pedro Pereira Lourenço <ti2grupopuc@gmail.com>
2021-11-30 21:09:16 -0300

    changed Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import buddyserver.Server.ServerObject;
 public class Main {
 public static void main(String[] args) {
 	
-		
+		System.out.println("Lines:");
 		for(int i = 0; i < args.length; i++) 
 		{
 			System.out.println(args[i]);
